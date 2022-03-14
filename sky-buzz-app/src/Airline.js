fc176d58a1206e5ecc5ebbdf8eb82b11fb145b81
malissab <malissambishop7@gmail.com>
2022-03-13 19:18:27 -0700

    adding airline components

--- sky-buzz-app/src/Airline.js
+++ sky-buzz-app/src/Airline.js
@@ -1,29 +1,66 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import Header from './Header'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`
+const Column = styled.div`
+  background: #fff;
+  height: 100vh;
+  overflow: scroll;
+
+  &:last-child {
+    background: #000;
+  }
+`
+const Main = styled.div`
+  padding-left: 50px;
+`
+const Reviews = styled.div``
+const ReviewForm = styled.div``
+
 
 function Airline() {
   const [airline, setAirline] = useState({})
   const [review, setReview] = useState({})
+  const [loaded, setLoaded] = useState(false)
 
   const { slug } = useParams();
 
   useEffect(() => {
     fetch(`/airlines/${slug}`)
-    .then(res => setAirline(res.data))
+    .then(res => res.json())
+    .then(data => {
+        setAirline(data)
+        setLoaded(true)
+    })
  
-  }, [])
+  }, [slug])
 
   return (
-    <div className="wrapper">
-      <div className='left-column'>
-        <div className='header'></div>
+    <Container>
+      <Column>
+        <Main>
+      {
+        loaded &&
+        <Header 
+          attributes={airline.data.attributes}
+          reviews={airline.included}
+        />
+      }
         <div className='reviews'></div>
-      </div>
-      <div className='right-column'>
-        <div className='review-form'></div>
-      </div>
+        </Main>
+      </Column>
+      <Column>
+        <div className='review-form'>a form</div>
+      </Column>
 
-    </div>
+    </Container>
   )
 }
 
--- sky-buzz-app/src/Header.js
+++ sky-buzz-app/src/Header.js
@@ -1,15 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
 
-function Header() {
+
+const Container = styled.div`
+    padding: 50px 100px 50px 0;
+    font-size: 30px;
+
+    img {
+        height: 70px;
+        width: 80px;
+        border-radius: 100%;
+        border: 1px solid rgba(0,0,0,0.1);
+        margin-bottom: -8px;
+    }
+`
+const TotalReviews = styled.div`
+    font-size: 18px;
+    padding: 10px 0;
+`
+const GrandTotal = styled.div`
+    font-size: 18px;
+    font-weight: bold;
+    padding: 10px 0;
+`
+
+function Header(props) {
+
+    const { name, image_url, avg_score } = props.attributes
+    const total = props.reviews.length
+
   return (
-    <div className='wrapper'>
-        <h1><img src=""/> Airline Name </h1>
+    <Container>
+        <h1><img src={image_url} alt={name}/> {name}</h1>
     <div>
-        <div className='totalReviews'></div>
-        <div className='starRating'></div>
-        <div className='totalOutOf'>3 out of 5</div>
+        <TotalReviews>{total} User Reviews</TotalReviews>
+        <div className='starRating'>5 out of 5</div>
+        <GrandTotal>{avg_score}</GrandTotal>
         </div>
-    </div>
+    </Container>
   )
 }
 
